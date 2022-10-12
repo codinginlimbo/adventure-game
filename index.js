0f0efa54feca80a169b9676318a217f45e130fdd
codinginlimbo <chrismercedes01@gmail.com>
2022-10-11 21:16:45 -0700

    made my first game

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 //Your Code here
+let firstAnswer = window.prompt("Do you head left or right?");
+
+//User selected left
+if (firstAnswer === "left") {
+    console.log("You selected left")
+let secondAnswer = window.prompt("Do you go to dead forest or school?");
+//User selects dead forest
+if(secondAnswer === "dead forest") {
+    console.log("Good choice, schools tuff")
+}
+//User selects school
+else if(secondAnswer === "school") {
+    console.log("Bad choice, dead forest has cooler picture spots")
+}
+//User selects anything else
+    {console.log("Game over! Try again")
+}
+}
+//User selects right
+if (firstAnswer === "right") {
+    console.log("You selected right")
+let secondAnswer = window.prompt("Do you go to clown school or tik tok school?");
+//User selects dead forest
+if(secondAnswer === "clown school") {
+    console.log("Good choice, clowns are really funny... or scary")
+}
+//User selects school
+else if(secondAnswer === "tik tok school") {
+    console.log("Bad choice, tik tok ppl have hella drama")
+}
+//User selects anything else
+    {console.log("Game over! Try again")
+}
+}
+
